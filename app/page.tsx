958d6b3db2c8e4963bc633790391a6413d8a219c
V <toriatovawebdev@gmail.com>
2024-11-11 18:28:18 +0400

    chore:setup linting configs

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-white font-black">DevFlow</h1>
+      <h1 className="text-3xl font-black text-white">DevFlow</h1>
     </div>
   );
-}
+};
 
 export default Home;
