168e6161de6ccd9b5be74614a9faf0e7831dc469
V <toriatovawebdev@gmail.com>
2024-11-22 15:02:10 +0400

    chore: setup fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl font-black text-white">DevFlow</h1>
+      <h1 className="text-3xl font-black text-black">DevFlow</h1>
+      <h1 className="font-space-grotesk text-3xl font-black text-black">
+        DevFlow
+      </h1>
     </div>
   );
 };
