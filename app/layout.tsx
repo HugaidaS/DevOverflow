127b16521eab9b16bb5e2ba72bd280e900dd4ce7
V <toriatovawebdev@gmail.com>
2024-12-06 13:58:21 +0400

    feat: add next-themes package to manage themes of the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 
+import ThemeProvider from "@/context/Theme";
+
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
   variable: "--font-inter",
@@ -31,11 +33,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl font-black text-black">DevFlow</h1>
-      <h1 className="font-space-grotesk text-3xl font-black text-black">
-        DevFlow
-      </h1>
+      <h1 className="text-3xl font-black">DevFlow</h1>
+      <h1 className="font-space-grotesk text-3xl font-black">DevFlow</h1>
     </div>
   );
 };
