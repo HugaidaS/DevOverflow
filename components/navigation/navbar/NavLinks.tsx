5ca49a8f90f8df7a4c3e6cdcdd3ceff9b391044b
V <toriatovawebdev@gmail.com>
2025-02-07 15:29:29 +0400

    feat: implement Mobile navbar and links

---
+++ components/navigation/navbar/NavLinks.tsx
@@ -0,0 +1,67 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+import { SheetClose } from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+
+const NavLinks = ({ isMobileNav = false }: { isMobileNav?: boolean }) => {
+  const pathName = usePathname();
+  const userId = "1234";
+  return (
+    <>
+      {sidebarLinks.map((link) => {
+        const isActive =
+          (pathName.includes(link.route) && link.route.length > 1) ||
+          pathName === link.route;
+
+        if (link.route === "/profile") {
+          if (userId) link.route = `/profile/${userId}`;
+          else return null;
+        }
+
+        const LinkComponent = (
+          <Link
+            href={link.route}
+            className={cn(
+              isActive
+                ? "primary-gradient rounded-lg text-light-900"
+                : "text-dark300_light900",
+              "flex items-center justify-start gap-5 bg-transparent p-4"
+            )}
+          >
+            <Image
+              src={link.imgURL}
+              alt={link.label}
+              width={20}
+              height={20}
+              className={cn({
+                "invert-colors": !isActive,
+              })}
+            />
+            <p
+              className={cn(
+                isActive ? "base-bold" : "base-medium",
+                !isMobileNav && "max-lg:hidden"
+              )}
+            >
+              {link.label}
+            </p>
+          </Link>
+        );
+
+        return isMobileNav ? (
+          <SheetClose asChild key={link.label}>
+            {LinkComponent}
+          </SheetClose>
+        ) : (
+          <React.Fragment key={link.label}> LinkComponent</React.Fragment>
+        );
+      })}
+    </>
+  );
+};
+export default NavLinks;
--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import MobileNavigation from "@/components/navigation/navbar/MobileNavigation";
 import ThemeToggle from "@/components/navigation/navbar/ThemeToggle";
 
 const Navbar = () => {
@@ -21,6 +22,7 @@ const Navbar = () => {
       <p>Global Search</p>
       <div className="flex-between gap-5">
         <ThemeToggle />
+        <MobileNavigation />
       </div>
     </nav>
   );
