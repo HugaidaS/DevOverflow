6227c29f8123aaa76efd93095e505d27df9762ec
V <toriatovawebdev@gmail.com>
2025-02-26 01:27:47 +0400

    feat: add dynamic retrieval of a tech icon from devicon, finalize right sidebar

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import { Badge } from "@/components/ui/badge";
 import ROUTES from "@/constants/routes";
+import { getDeviconClassName } from "@/lib/utils";
 
 interface TagCardProps {
   _id: string;
@@ -19,14 +20,19 @@ const TagCard = ({
   showCount = true,
   compact = false,
 }: TagCardProps) => {
+  const iconClass = getDeviconClassName(name);
   return (
     <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
-      <Badge className="background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
         <div className="flex-center space-x-2">
-          <i>ICON</i>
+          <i className={`${iconClass} text-sm`}></i>
           <span>{name}</span>
         </div>
       </Badge>
+
+      {showCount && (
+        <p className="small-medium text-dark500_light700">{questions}</p>
+      )}
     </Link>
   );
 };
