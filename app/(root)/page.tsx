612ba2e630f2346653691af86ed89b6940912848
“V” <toriatovawebdev@gmail.com>
2025-04-14 17:43:18 +0400

    feat(home): draft the structure of the Home layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,30 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-black">DevFlow</h1>
-      <h1 className="font-space-grotesk text-3xl font-black">DevFlow</h1>
-    </div>
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="text-dark100_light900 h1-bold">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">LocalSearch</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
+    </>
   );
 };
 
