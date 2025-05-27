d22997796a62c969fbc7724960f28a810d9253e8
“V” <toriatovawebdev@gmail.com>
2025-05-28 02:09:29 +0400

    feat(editor): integrate MDXEditor with dynamic import and dark theme support

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import React from "react";
+import { MDXEditorMethods } from "@mdxeditor/editor";
+import dynamic from "next/dynamic";
+import React, { useRef } from "react";
 import { useForm } from "react-hook-form";
 
 import { Button } from "@/components/ui/button";
@@ -17,7 +19,14 @@ import {
 import { Input } from "@/components/ui/input";
 import { AskQuestionSchema } from "@/lib/validations";
 
+const Editor = dynamic(() => import("@/components/editor"), {
+  // Make sure we turn SSR off
+  ssr: false,
+});
+
 const QuestionForm = () => {
+  const editorRef = useRef<MDXEditorMethods>(null);
+
   const form = useForm({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
@@ -70,7 +79,13 @@ const QuestionForm = () => {
                   Detailed description of your problem{" "}
                   <span className="text-primary-500">*</span>
                 </FormLabel>
-                <FormControl>Editor</FormControl>
+                <FormControl>
+                  <Editor
+                    editorRef={editorRef}
+                    value={field.value}
+                    fieldChange={field.onChange}
+                  />
+                </FormControl>
                 <FormDescription className="body-regular mt-2.5 text-light-500">
                   Introduce the problem and expand on what you put in the title.
                 </FormDescription>
